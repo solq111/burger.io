d7f2ad2ac76fa79675f7b41c3a43434adbf29455
Solq111 <solq111@protonmail.com>
2020-07-30 20:02:34 +0200

    bla

--- js/about.js
+++ js/about.js
@@ -8,16 +8,24 @@ let users = [
     {surename: "Nagy", firstname: "Peter", age: 33},
 ]
 
+/* Mukodik a meat lover-ra
 function calcAmount() {
     let price = 990;
     let quantityInput = document.querySelector("input.burgerQuantity");
     let showAmount = document.querySelector("span.show-amount");
     let payable = parseInt(quantityInput.value) * price;
     showAmount.innerHTML = payable;
-}
+} */
 
-
-let items = ["Bottle", 4, true];
-        items.push("Juice");
-        console.log(items);
-
+function calcAmount() {
+    let price = 990;
+    let quantityInput = document.querySelectorAll("input.burgerQuantity");
+    let number = 0;
+    for (let i = 0; i < quantityInput.length; i++) {
+        number = number + parseInt((quantityInput[i].value));
+        let value = number * price;
+        let showAmount = document.querySelector("span.show-amount");
+        showAmount.innerHTML = value;}}
+        ;
+        
+       // if (isNaN(quantityInput)){quantityInput=0};
